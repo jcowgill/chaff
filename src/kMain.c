e3cb10af60421fed02eb8f974960057ac7e1fc1f
James Cowgill <james410@cowgill.org.uk>
2011-01-06 22:16:55 +0000

    Added test code which will be removed later.

--- src/kMain.c
+++ src/kMain.c
@@ -11,6 +11,47 @@
 #include "process.h"
 #include "interrupt.h"
 
+void NORETURN test(void * arg)
+{
+	bool t1;
+
+	//Test arg
+	if((unsigned int) arg == 1234)
+	{
+		t1 = true;
+		PrintLog(Info, "Test thread 1 started");
+	}
+	else if((unsigned int) arg == 5678)
+	{
+		t1 = false;
+		PrintLog(Info, "Test thread 2 started");
+	}
+	else
+	{
+		Panic("Invalid test argument");
+	}
+
+	for(;;)
+	{
+		//Do some crap
+		if(t1)
+		{
+			PrintLog(Notice, "Some crap from test 1");
+		}
+		else
+		{
+			PrintLog(Notice, "Some crap from test 2");
+		}
+
+		//Wait an obseen amount of time
+		for(int i = 100000; i > 0; --i)
+			;
+
+		//Yeild
+		ProcYield();
+	}
+}
+
 void NORETURN kMain(unsigned int mBootCode, multiboot_info_t * mBootInfo)
 {
 	//Kernel C Entry Point
@@ -32,7 +73,18 @@ void NORETURN kMain(unsigned int mBootCode, multiboot_info_t * mBootInfo)
 	// Initialize process manager and scheduler
 	ProcInit();
 
-	for(;;);
+#warning IDEAS
+	//Idea - have drives load from a kernel thread so they don't have
+	// to deal with the boot special case (ProcCurrThread == NULL)
+#warning Why not have a kernel thread return address?
+
+	//DEBUG
+	// Setup some testing threads
+	ProcWakeUp(ProcCreateKernelThread("test2", test, (void *) 5678));
+	ProcWakeUp(ProcCreateKernelThread("test1", test, (void *) 1234));
+
+	// Exit boot mode
+	ProcExitBootMode();
 }
 
 void NORETURN Panic(const char * msg, ...)
@@ -53,7 +105,7 @@ void PrintStr(const char * msg)
 	//Prints a string to the video output
 	while(*msg)
 	{
-		*nextPos++ = *msg;
+		*nextPos++ = *msg++;
 		*nextPos++ = 7;
 
 		//Wrap around if reached the end
@@ -102,7 +154,7 @@ void PrintLog(LogLevel level, const char * msg, ...)
 	PrintStr(msg);
 
 	//New line + wrap around
-	nextPos += 50 - (((unsigned int) nextPos) % 50);
+	nextPos += 160 - ((((unsigned int) nextPos) - 0xC00B8000) % 160);
 	if((unsigned int) nextPos >= 0xC00B8FA0)
 	{
 		nextPos = (char *)0xC00B8000;
