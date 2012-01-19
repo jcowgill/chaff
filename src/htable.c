22c0e9c543392178a4b6bb6427ceb595fe44d55b
James Cowgill <james410@cowgill.org.uk>
2012-01-19 23:47:58 +0000

    Added alternate string hash table.
    - This hash table will also handle using null-terminated and strings with lengths at the same time

--- src/htable.c
+++ src/htable.c
@@ -24,6 +24,10 @@
 
 //Simple 256 bucket hash table
 
+static bool HashTableStringMixedCompare(HashTableString * table, bool usingLen,
+		const char * str, int len, HashItem * other);
+static unsigned int HashTableStringHash(bool usingLen, const char * str, int len);
+
 //Insert an item into the hashmap
 // You must set the ID in the HashItem
 // Returns false if that ID already exists
@@ -137,14 +141,117 @@ HashItem * HashTableFind(HashTable * table, ...)
 	return NULL;
 }
 
+//Insert an item into the string hashmap
+// You must set the ID in the HashItem
+// Returns false if that ID already exists
+bool HashTableStringInsert(HashTableString * table, HashItem * item)
+{
+	//Get key
+	const char * str;
+	int len;
+	bool usingLen = table->key(item, &str, &len);
+
+	//Get hash
+	unsigned int hash = HashTableStringHash(usingLen, str, len);
+
+	//Get bucket
+	HashItem * currItem = table->table[hash];
+
+	//Setup new item
+	item->next = currItem;
+
+	//Check if in table
+	while(currItem != NULL)
+	{
+		if(HashTableStringMixedCompare(table, usingLen, str, len, currItem))
+		{
+			//Already in hash table
+			return false;
+		}
+
+		currItem = currItem->next;
+	}
+
+	//Insert at beginning
+	table->table[hash] = item;
+	table->count++;
+	return true;
+}
+
+//Removes the given item from the string hash table
+// Returns false if that ID doesn't exist
+bool HashTableStringRemoveGeneric(HashTableString * table, bool usingLen, const char * str, int len)
+{
+	//Get bucket for hash
+	unsigned int hash = HashTableStringHash(usingLen, str, len);
+
+	HashItem ** ptrToItem = &table->table[hash];
+	HashItem * currItem = *ptrToItem;
+
+	//Find item
+	while(currItem != NULL)
+	{
+		if(HashTableStringMixedCompare(table, usingLen, str, len, currItem))
+		{
+			//Change pointer to item to this item's next value
+			*ptrToItem = currItem->next;
+			currItem->next = NULL;
+			table->count--;
+			return true;
+		}
+
+		//Next item
+		ptrToItem = &currItem->next;
+		currItem = *ptrToItem;
+	}
+
+	//Not found
+	return false;
+}
+
+//Removes the given item from the hash table
+// Returns false if that ID doesn't exist
+bool HashTableStringRemoveItem(HashTableString * table, HashItem * item)
+{
+	const char * str;
+	int len;
+	bool usingLen = table->key(item, &str, &len);
+
+	return HashTableStringRemoveGeneric(table, usingLen, str, len);
+}
+
+//Returns the HashItem corresponding to a given string
+// Returns NULL if that ID doesn't exist
+HashItem * HashTableStringFindGeneric(HashTableString * table, bool usingLen, const char * str, int len)
+{
+	//Get bucket for hash
+	unsigned int hash = HashTableStringHash(usingLen, str, len);
+
+	HashItem * item = table->table[hash];
+
+	//Find the item, starting with the first bucket
+	while(item != NULL)
+	{
+		//Chrck ID
+		if(HashTableStringMixedCompare(table, usingLen, str, len, item))
+		{
+			//Found item
+			return item;
+		}
+
+		//Next item
+		item = item->next;
+	}
+
+	//Item not found
+	return NULL;
+}
+
+
+//Hash related functions
 unsigned int HashTableIntHash(const void * num)
 {
 	return HashTableIntHashHelp((unsigned int) num);
-}
-
-unsigned int HashTableStrHash(const void * str)
-{
-	return HashTableStrHashHelp(str);
 }
 
 unsigned int HashTableMemHash(const void * memIn, unsigned int size)
@@ -167,7 +274,52 @@ bool HashTableCompare(const void * num1, const void * num2)
 	return num1 == num2;
 }
 
-bool HashTableStrCompare(const void * inStr1, const void * inStr2)
+//Internal string helpers
+
+//Compares a hash item which possibly uses mixed types (null terminated and length given)
+static bool HashTableStringMixedCompare(HashTableString * table, bool usingLen,
+		const char * str, int len, HashItem * other)
 {
-	return StrCmp((const char *) inStr1, (const char *) inStr2) == 0;
+	//Get other key
+	const char * oStr;
+	int oLen;
+
+	if(table->key(other, &oStr, &oLen))
+	{
+		//Other uses len
+		if(!usingLen)
+		{
+			len = StrLen(str);
+		}
+	}
+	else
+	{
+		//Other uses zterm
+		if(usingLen)
+		{
+			//Do a memory comparison
+			oLen = StrLen(oStr);
+		}
+		else
+		{
+			//Quicker than doing 2 strlens
+			return StrCmp(str, oStr) == 0;
+		}
+	}
+
+	//If we're here, both are using lengths so we can do a memory compraison
+	return len == oLen && MemCmp(str, oStr, len) == 0;
+}
+
+//Hashes the given string
+static unsigned int HashTableStringHash(bool usingLen, const char * str, int len)
+{
+	if(usingLen)
+	{
+		return HashTableMemHash(str, len) % HASHT_BUCKET_COUNT;
+	}
+	else
+	{
+		return HashTableMemHash(str, StrLen(str)) % HASHT_BUCKET_COUNT;
+	}
 }
--- src/include/htable.h
+++ src/include/htable.h
@@ -46,6 +46,19 @@ typedef struct
 
 } HashTable;
 
+//String hash table which will also handle strings specified by length
+// instead of being null terminated
+typedef struct
+{
+	//Gets the key (and possibly length) for a string hash item
+	// Returns true if length is used. Otherwise str must be null terminated.
+	bool (* key)(HashItem * item, const char ** str, int * len);
+
+	HashItem * table[HASHT_BUCKET_COUNT];
+	int count;
+
+} HashTableString;
+
 //Insert an item into the hashmap
 // You must set the ID in the HashItem
 // Returns false if that ID already exists
@@ -63,13 +76,56 @@ bool HashTableRemoveItem(HashTable * table, HashItem * item);
 // Returns NULL if that ID doesn't exist
 HashItem * HashTableFind(HashTable * table, ...);
 
+
+//Insert an item into the string hashmap
+// You must set the ID in the HashItem
+// Returns false if that ID already exists
+bool HashTableStringInsert(HashTableString * table, HashItem * item);
+
+//Generic remove - dont use this, use one of the other removers
+bool HashTableStringRemoveGeneric(HashTableString * table, bool usingLen, const char * str, int len);
+
+//Removes the given item from the string hash table (using null terminated string)
+// Returns false if that ID doesn't exist
+static inline bool HashTableStringRemove(HashTableString * table, const char * str)
+{
+	return HashTableStringRemoveGeneric(table, false, str, 0);
+}
+
+//Removes the given item from the string hash table (using string length)
+// Returns false if that ID doesn't exist
+static inline bool HashTableStringRemoveLen(HashTableString * table, const char * str, int len)
+{
+	return HashTableStringRemoveGeneric(table, true, str, len);
+}
+
+//Removes the given item from the hash table
+// Returns false if that ID doesn't exist
+bool HashTableStringRemoveItem(HashTableString * table, HashItem * item);
+
+//Generic find - don't use this, use one of the other removers
+HashItem * HashTableStringFindGeneric(HashTableString * table, bool usingLen, const char * str, int len);
+
+//Returns the HashItem corresponding to a given string (using null terminated string)
+// Returns NULL if that ID doesn't exist
+static inline HashItem * HashTableStringFind(HashTableString * table, const char * str)
+{
+	return HashTableStringFindGeneric(table, false, str, 0);
+}
+
+//Returns the HashItem corresponding to a given string (using string length)
+// Returns NULL if that ID doesn't exist
+static inline HashItem * HashTableStringFindLen(HashTableString * table, const char * str, int len)
+{
+	return HashTableStringFindGeneric(table, false, str, len);
+}
+
+
 //Gets the hash tale item corresponding to a given HashItem
 #define HashTableEntry ListEntry
 
 //Helper hash functions
 unsigned int HashTableIntHash(const void * num);
-unsigned int HashTableStrHash(const void * str);
-
 unsigned int HashTableMemHash(const void * mem, unsigned int size);
 
 static inline unsigned int HashTableIntHashHelp(unsigned int num)
@@ -77,12 +133,7 @@ static inline unsigned int HashTableIntHashHelp(unsigned int num)
 	return HashTableMemHash(&num, sizeof(unsigned int));
 }
 
-static inline unsigned int HashTableStrHashHelp(const char * str)
-{
-	return HashTableMemHash(str, StrLen(str));
-}
-
-bool HashTableCompare(const void * num1, const void * num2);		//Compares the pointers given - use for ints and ptrs
-bool HashTableStrCompare(const void * str1, const void * str2);		//Compares the strings given
+//Compares the pointers given - use for ints and ptrs
+bool HashTableCompare(const void * num1, const void * num2);
 
 #endif /* HTABLE_H_ */
